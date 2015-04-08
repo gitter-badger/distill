6e07f6d7e0fec5d984736a3b5e0b583bd0bf8022
Ralph Schindler <ralph@ralphschindler.com>
2015-04-08 14:15:52 -0500

    Improved tests for router

--- tests/Router/HttpRouteTest.php
+++ tests/Router/HttpRouteTest.php
@@ -1,25 +1,152 @@
 <?php
-/**
- * Project distillphp.
- * User: ralphschindler
- * Date: 8/12/14
- * Time: 9:31 AM
- */
 
 namespace Distill\Test\Router;
-
 
 use Distill\Router\HttpRoute;
 
 class HttpRouteTest extends \PHPUnit_Framework_TestCase
 {
-    public function testMatch()
-    {
-        $cliRoute = new HttpRoute('GET /', function () { return true; });
-        $this->assertEquals([], $cliRoute->match(['uri' => '/', 'method' => 'GET']));
 
-        $cliRoute = new HttpRoute('/:name', function () { return true; });
-        $this->assertEquals(['name' => 'ralph'], $cliRoute->match(['uri' => '/ralph', 'method' => 'GET']));
+    /**
+     * @dataProvider matchDataProvider
+     */
+    public function testMatch($ctorArgs, $matchParams, $equalTo)
+    {
+        $cliRoute = new HttpRoute(
+            $ctorArgs[0],
+            function () { return true; },
+            (isset($ctorArgs[1])) ? $ctorArgs[1] : [],
+            (isset($ctorArgs[2])) ? $ctorArgs[2] : [],
+            (isset($ctorArgs[3])) ? $ctorArgs[3] : true
+        );
+        $output = $cliRoute->match($matchParams);
+        $this->assertEquals($equalTo, $output);
+    }
+
+    public function matchDataProvider()
+    {
+        return [
+            // match root
+            [
+                ['GET /'],
+                ['uri' => '/', 'method' => 'GET'],
+                []
+            ],
+            // match root with required param, provided
+            [
+                ['/:name'],
+                ['uri' => '/ralph', 'method' => 'GET'],
+                ['name' => 'ralph']
+            ],
+            // match root with required param, not provided
+            [
+                ['/:name'],
+                ['uri' => '/', 'method' => 'GET'],
+                false
+            ],
+            // match root with optional param, provided
+            [
+                ['/[:name]'],
+                ['uri' => '/ralph', 'method' => 'GET'],
+                ['name' => 'ralph']
+            ],
+            // match root with optional param, not provided
+            [
+                ['/[:name]'],
+                ['uri' => '/', 'method' => 'GET'],
+                []
+            ],
+            // match literal with optional param, not provided
+            [
+                ['/hello[/:name]'],
+                ['uri' => '/hello', 'method' => 'GET'],
+                []
+            ],
+            // match literal with optional param, provided
+            [
+                ['/hello[/:name]'],
+                ['uri' => '/hello/ralph', 'method' => 'GET'],
+                ['name' => 'ralph']
+            ],
+            // match literal and required param, not provided
+            [
+                ['/hello/:name'],
+                ['uri' => '/hello', 'method' => 'GET'],
+                false
+            ],
+            // match optional param with validation, provided
+            [
+                ['/hello[/:name#ralph|joe#]'],
+                ['uri' => '/hello/joe', 'method' => 'GET'],
+                ['name' => 'joe']
+            ],
+            // match optional param with validation, provided, but fails validation
+            [
+                ['/hello[/:name#ralph|joe#]'],
+                ['uri' => '/hello/foo', 'method' => 'GET'],
+                false
+            ],
+            // match required param with validation, provided
+            [
+                ['/hello/:name#ralph|joe#'],
+                ['uri' => '/hello/ralph', 'method' => 'GET'],
+                ['name' => 'ralph']
+            ],
+            // match required param with validation, not provided
+            [
+                ['/hello/:name#ralph|joe#'],
+                ['uri' => '/hello', 'method' => 'GET'],
+                false
+            ],
+            // match required param with regex validation, provided
+            [
+                ['/hello/:id#\d#'],
+                ['uri' => '/hello/5', 'method' => 'GET'],
+                ['id' => '5']
+            ],
+            // match required param with regex validation, provided
+            [
+                ['/hello/:id#\d#'],
+                ['uri' => '/hello/thing', 'method' => 'GET'],
+                false
+            ],
+            // match literal, no implied trailing slash
+            [
+                ['/hello', [], [], false],
+                ['uri' => '/hello/', 'method' => 'GET'],
+                false
+            ],
+            // match literal with optional parameter, no implied trailing slash
+            [
+                ['/hello[/:name]', [], [], false],
+                ['uri' => '/hello/', 'method' => 'GET'],
+                false
+            ],
+            // match literal with optional parameter, no implied trailing slash
+            [
+                ['/hello[/:name]', [], [], false],
+                ['uri' => '/hello/ralph/', 'method' => 'GET'],
+                false
+            ],
+            // match literal with optional param, with default
+            [
+                ['/hello[/:name]', ['name' => 'ralph'], []],
+                ['uri' => '/hello/', 'method' => 'GET'],
+                ['name' => 'ralph']
+            ],
+            // match literal with optional param, with default
+            [
+                ['/hello/:name', ['name' => 'ralph'], []],
+                ['uri' => '/hello/', 'method' => 'GET'],
+                false
+            ],
+            // match literal with required param, with provided validator
+            [
+                ['/hello/:id', [], ['id' => ['#\d#']]],
+                ['uri' => '/hello/foo', 'method' => 'GET'],
+                false
+            ]
+        ];
     }
 }
  
